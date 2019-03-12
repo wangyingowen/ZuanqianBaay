c0611349488e927e7584d9963fc07cf1cc8f9aa3
wangyingowen <wy250588812@qq.com>
2019-03-12 14:55:27 +0800

    添加内容

---
+++ app/src/main/java/cn/tsthuah/zuanqianbaay/LoadingActvity.java
@@ -0,0 +1,5 @@
+package cn.tsthuah.zuanqianbaay;
+
+public class LoadingActvity {
+
+}
--- app/src/main/java/cn/tsthuah/zuanqianbaay/MainActivity.java
+++ app/src/main/java/cn/tsthuah/zuanqianbaay/MainActivity.java
@@ -3,6 +3,9 @@ package cn.tsthuah.zuanqianbaay;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * wangying
+ */
 public class MainActivity extends AppCompatActivity {
 
   @Override
