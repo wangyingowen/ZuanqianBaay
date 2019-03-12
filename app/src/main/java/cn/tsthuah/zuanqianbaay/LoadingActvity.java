02ead26961f83f60911e9021636c7351455d7c06
wangyingowen <wy250588812@qq.com>
2019-03-12 17:50:57 +0800

    透明标题栏

--- app/src/main/java/cn/tsthuah/zuanqianbaay/LoadingActvity.java
+++ app/src/main/java/cn/tsthuah/zuanqianbaay/LoadingActvity.java
@@ -1,8 +1,11 @@
 package cn.tsthuah.zuanqianbaay;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.Window;
 
 /**
  * @author wy
@@ -12,5 +15,25 @@ public class LoadingActvity extends Activity{
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    setContentView(R.layout.loading_actvity);
+    View topView = findViewById(R.id.iii);
+             ImmersedStatusbarUtils.initAfterSetContentView(this, topView);
+
+             Thread thread=new Thread(new Runnable() {
+               @Override
+               public void run() {
+                 try {
+                   Thread.sleep(2000);
+                   Intent intent=new Intent(LoadingActvity.this,MainActivity.class);
+                   startActivity(intent);
+                 } catch (InterruptedException e) {
+                   e.printStackTrace();
+                 }
+
+               }
+             });
+
+             thread.start();
   }
 }
--- app/src/main/java/cn/tsthuah/zuanqianbaay/MainActivity.java
+++ app/src/main/java/cn/tsthuah/zuanqianbaay/MainActivity.java
@@ -1,7 +1,12 @@
 package cn.tsthuah.zuanqianbaay;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * wangying
@@ -11,6 +16,47 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     setContentView(R.layout.activity_main);
+
+    hideStatusBar();
   }
+
+
+  //只透明状态栏
+  private void hideStatusBar() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      Window window = getWindow();
+      window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+      window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+      window.setStatusBarColor(Color.TRANSPARENT);
+      return;
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
+  }
+
+  //状态栏、导航栏都透明
+  private void hideStatusBarNavigationBar() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      Window window = getWindow();
+      window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+          | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+      window.getDecorView().setSystemUiVisibility(
+          View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+      window.setStatusBarColor(Color.TRANSPARENT);
+      window.setNavigationBarColor(Color.TRANSPARENT);
+      return;
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+    }
+  }
+
 }
